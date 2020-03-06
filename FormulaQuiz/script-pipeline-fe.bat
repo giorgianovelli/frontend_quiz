e2fe107ad46cf5037056e89e08c2e0eaaf918d68
giorgianovelli <32876200+giorgianovelli@users.noreply.github.com>
2020-03-06 23:41:40 +0100

    Delete script-pipeline-fe.bat

--- FormulaQuiz/script-pipeline-fe.bat
+++
@@ -1,4 +0,0 @@
-npm run build --prod
-SET AWS_ACCESS_KEY_ID=AKIASTNGBHQKFOUNGXU7
-SET AWS_SECRET_ACCESS_KEY=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
-aws s3 sync dist/FormulaQuiz s3://frontend-quiz/ --acl=public-read --delete --exclude '.git/*'
